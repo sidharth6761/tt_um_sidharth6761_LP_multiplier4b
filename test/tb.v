c2cde760f3018cf78fb3cb7ae71164b3fa23d8cb
S.Sidharth <ssidharth6761@gmail.com>
2026-03-11 15:21:11 +0530

    Update tb.v

--- test/tb.v
+++ test/tb.v
@@ -28,7 +28,7 @@ module tb ();
 `endif
 
   // Replace tt_um_example with your module name:
-  tt_um_sidharth6761_LP_multiplier4b (
+  multiplier (
 
       // Include power ports for the Gate Level test:
 `ifdef GL_TEST
